4c8bc5f704743f81eee56d9a715ca20c0f672058
daryagolubeva <darya.golubewa@gmail.com>
2018-10-24 09:54:28 +0300

    attributes to ShippingContainer() added;

--- part-1/ShippingContainer.js
+++ part-1/ShippingContainer.js
@@ -1,2 +1,8 @@
 class ShippingContainer {
+	constructor(args={}) {
+		this.destination = args.destination;
+		this.boxes = args.boxes;
+		this.maxWeight = args.maxWeight;
+		this.maxBoxs = args.maxBoxs; 
+	}
 }
